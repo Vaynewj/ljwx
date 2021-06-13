d945e0326956ade593a63e4a16edd8b3269d9962
Vaynewj <83951949+Vaynewj@users.noreply.github.com>
2021-06-13 09:12:54 +0800

    Add files via upload

---
+++ jpjslfm.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <div class="btn">
+      <span class="register">注册</span>
+      <span>取消</span>
+    </div>
+    <div class="hi">
+      Hi,Kris <br> 欢迎回来！
+    </div>
+    <div class="signIn">
+      <input class="sr" type="text" value="" placeholder="请输入您的账号">
+    </div>
+    <div class="signIn">
+      <input class="sr sr1" :type="active?'text':'password'" value="" placeholder="请输入您的密码">
+      <img class="getTp" src="../assets/images/ims.png" alt=""  @click="toggle">
+    </div>
+    <div class="signInB" :class="success?'success':''" @click="toggle1">
+      登录
+    </div>
+    <div class="land" @click="skip">
+      使用验证码登录
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'jpjslfm',
+  components: {
+
+  },
+  data () {
+    return {
+      active: false,
+      success: false
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    toggle () {
+      this.active = !this.active
+    },
+    toggle1 () {
+      this.success = !this.success
+    },
+    skip () {
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.btn{
+  margin: 24px 50px;
+  span{
+  font-size: 28px;
+  font-weight: 500;
+  line-height: 22px;
+  color: #737F97;
+  display: block;
+  }
+  .register{
+    color: #3861FB;
+    float: right;
+  }
+}
+.hi{
+    margin: 84px 50px 0;
+    font-size: 56px;
+    font-weight: 600;
+    line-height: 80px;
+    color: #000000;
+}
+.signIn{
+  position: relative;
+  .sr{
+    margin: 74px auto 14px;
+    width: 650px;
+    height: 66px;
+    font-size: 30px;
+    font-weight: 500;
+    line-height: 42px;
+    color: #000000;
+    border: 0;
+    border-bottom: 1px solid #F7F6FB;
+    display: block;
+  }
+  .sr1{
+      margin-top: 70px;
+  }
+  .getTp{
+    position: absolute;
+    top: 16px;
+    right: 25px;
+    width: 32px;
+    height: 32px;
+    background: #FFFFFF;
+  }
+}
+.signInB{
+  margin: 80px 50px 0;
+  width: 650px;
+  height: 80px;
+  background: #3861fb;
+  border-radius: 12px;
+  font-size: 30px;
+  font-weight: 500;
+  line-height: 42px;
+  color: #FFFFFF;
+  text-align: center;
+  line-height: 80px;
+}
+.success{
+  background: #FFC1CBDF;
+  color: #fff;
+}
+.land{
+  margin-top: 34px;
+  font-size: 28px;
+  font-weight: 400;
+  line-height: 40px;
+  color: #000000;
+  text-align: center;
+}
+</style>
---
+++ login.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <div class="loginBtn">
+        <span class="cancel">注册</span>
+        <span>取消</span>
+    </div>
+    <div class="loginWrap">
+      <span class="signIn">登录</span>
+      <span class="welcome">欢迎来到,矿多多</span>
+      <div>
+        <input type="text" value="" placeholder="请输入电话号码">
+      </div>
+      <div>
+        <input type="text" value="" placeholder="请输入验证码">
+        <i class="getCode" :class="active?'active':''" @click="send">获取验证码</i>
+      </div>
+      <div class="signInBtn" :class="success?'success':''" @click="signIn">
+        登录
+      </div>
+      <div class="signIb" @click="skip">
+        使用密码登录
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  components: {
+
+  },
+  data () {
+    return {
+      active: false,
+      success: false
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    send () {
+      this.active = !this.active
+    },
+    signIn () {
+      this.success = !this.success
+    },
+    skip () {
+      this.$router.push('/jpjslfm')
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.loginBtn{
+  margin: 24px 50px;
+  span{
+    font-size: 28px;
+    font-weight: 500;
+    color: #737F97;
+    display: block;
+  }
+  .cancel{
+    color: #3861FB;
+    float: right;
+  }
+}
+.loginWrap{
+  margin: 84px 50px 0;
+  .signIn{
+    font-size: 56px;
+    font-weight: 600;
+    line-height: 40px;
+    color: #000000;
+    &::after{
+      content: '';
+      width: 58px;
+      height: 10px;
+      background: #3861FB;
+      display: block;
+      border-radius: 10px;
+    }
+  }
+  .welcome{
+    margin: 14px 0 46px;
+    font-size: 26px;
+    font-weight: 500;
+    line-height: 22px;
+    color: #C1CBDF;
+  }
+  div{
+    position: relative;
+    margin-top: 70px;
+    input{
+      width: 650px;
+      height: 66px;
+      font-size: 30px;
+      font-weight: 500;
+      line-height: 42px;
+      color: #000000;
+      border: 0;
+      border-bottom: 1px solid #F7F6FB;
+    }
+    .getCode{
+      position: absolute;
+      top: 14px;
+      right: 0;
+      width: 140px;
+      height: 52px;
+      background: #FFFFFF;
+      border: 2px solid #3861FB;
+      border-radius: 198px;
+      font-size: 22px;
+      font-weight: 400;
+      line-height: 32px;
+      color: #3861FB;
+      font-style: normal;
+      text-align: center;
+      line-height: 52px;
+    }
+    .active{
+      background: #3861FB;
+      color: #fff;
+    }
+  }
+  .signInBtn{
+    width: 650px;
+    height: 80px;
+    background: #C1CBDF;
+    border-radius: 12px;
+    text-align: center;
+    font-size: 30px;
+    font-weight: 500;
+    line-height: 42px;
+    color: #FFFFFF;
+    line-height: 80px;
+  }
+  .success{
+    background: #3861FB;
+    color: #fff;
+  }
+  .signIb{
+    margin-top: 34px;
+    font-size: 28px;
+    font-weight: 400;
+    line-height: 40px;
+    color: #000000;
+    text-align: center;
+  }
+}
+</style>
